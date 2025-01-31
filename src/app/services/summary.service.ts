a0e5b125f3edfffb7c26d99ee01780029ccf0592
Damngz <da.munozgz@gmail.com>
2025-01-31 09:40:34 -0300

    feat/monitoring

---
+++ src/app/services/summary.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Measurement } from './measurement.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SummaryService {
+  private apiUrl = 'http://localhost:8083/send';
+
+  constructor(private http: HttpClient) {}
+
+  sendSummary(summary: Measurement[]): Observable<any[]> {
+    console.log('enviando summary: ', summary);
+    return this.http.post<any[]>(this.apiUrl, summary);
+  }
+}
